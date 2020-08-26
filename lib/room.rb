e9d2e6bad269dd75497c35ea22cecdc88b689e91
Eugene Theriault <hybridbassist@gmail.com>
2020-08-26 08:14:19 -0700

    Add code for paint methods

--- lib/room.rb
+++ lib/room.rb
@@ -1,14 +1,23 @@
 class Room
-  attr_reader :category, :length, :width
+  attr_reader :category, :length, :width, :painted
 
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width.to_i
+    @painted = false
   end
 
   def area
     @length * @width
+  end
+
+  def is_painted?
+    @painted
+  end
+
+  def paint
+    @painted = true
   end
 
 end
