6288861d2adc29896986629776057497fa009ebd
Eugene Theriault <hybridbassist@gmail.com>
2020-08-26 08:45:56 -0700

    Add code and test for getting rooms by category

--- lib/house.rb
+++ lib/house.rb
@@ -15,4 +15,8 @@ class House
     @price.delete("$").to_i > 500000
   end
 
+  def rooms_from_category(category)
+    @rooms.select {|room| room.category == category}
+  end
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -5,6 +5,8 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, "13")
     @room_2 = Room.new(:bedroom, 11, "15")
+    @room_3 = Room.new(:living_room, 25, "15")
+    @room_4 = Room.new(:basement, 30, "41")
   end
 
   def test_it_exists
@@ -29,6 +31,16 @@ class HouseTest < Minitest::Test
     assert_equal false, @house.above_market_average?
     big_house = House.new("$1000000", "555 Expensive Drive")
     assert_equal true, big_house.above_market_average?
+  end
+
+  def test_can_get_rooms_from_category
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
+    assert_equal [@room_3], @house.rooms_from_category(:living_room)
   end
 
 end
